cf92d133771fb11891889ae902d128580aa29e64
Maurício Vieira <mauriciomvdr@hotmail.com>
2021-06-28 08:19:26 -0300

    feat(utils): creates utils functions and colors set

---
+++ src/config/colors.js
@@ -0,0 +1,46 @@
+const colors = {
+  thunderstorm: {
+    primary: '#222f61',
+    secondary: '#38436c',
+    primaryText: '#424242',
+    secondaryText: '#d7cca4',
+    tertiaryText: '#202949',
+  },
+  drizzle: {
+    primary: '#bfcbd7',
+    secondary: '#ccd5e0',
+    primaryText: '#587280',
+    secondaryText: '#95a8b5',
+    tertiaryText: '#8da2b8',
+  },
+  rain: {
+    primary: '#bfcbd7',
+    secondary: '#ccd5e0',
+    primaryText: '#587280',
+    secondaryText: '#95a8b5',
+    tertiaryText: '#8da2b8',
+  },
+  snow: {
+    primary: '#e1effc',
+    secondary: '#eafbfe',
+    primaryText: '#628c9f',
+    secondaryText: '#7598ac',
+    tertiaryText: '#8ac1f3',
+  },
+  clear: {
+    primary: '#ffe591',
+    secondary: '#fffeae',
+    primaryText: '#c67a05',
+    secondaryText: '#ce9e4f',
+    tertiaryText: '#9e6204',
+  },
+  clouds: {
+    primary: '#f3f6f0',
+    secondary: '#fcfcfc',
+    primaryText: '#4a4a4a',
+    secondaryText: '#a3a3a3',
+    tertiaryText: '#c2d2b3',
+  },
+};
+
+export default colors;
